f0dd4e366c2420505e9f1376ea1bc2ffe1e0991d
priscilla <priscillakung@gmail.com>
2023-06-08 14:47:54 -0700

    updated the story UI programmatically with an updateUI() method

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -8,35 +8,49 @@
 
 import UIKit
 
+var storyNumber = 0 //keeps track of choices
+
+let stories = [
+    Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."),
+    Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
+    Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps.")
+]
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
+    
     @IBAction func choiceMade(_ sender: UIButton) {
-        if sender.currentTitle == choice1 {
-            updateUI()
-        }
+       
+        let userChoice = sender.currentTitle!
         
-    }
-    
-    var storyStep = 0
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-//        storyLabel.text = story0
-//        choice1Button.setTitle(choice1, for: .normal)
-//        choice2Button.setTitle(choice2, for: .normal)
+        if userChoice == stories[storyNumber].choice1 {
+            storyNumber += 1
+        } else {
+            storyNumber += 2
+        }
         updateUI()
     }
     
-    @objc func updateUI() {
-        //change storyLabel
-        //change buttons title
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        storyLabel.text = stories[storyNumber].title
+        choice1Button.setTitle("Take a left.", for: .normal)
+        choice2Button.setTitle("Take a right.", for: .normal)
     }
     
-    func nextStoryLabel() {
-        
+    @objc func updateUI() {
+        storyLabel.text = stories[storyNumber].title //changes story label
+        choice1Button.setTitle(stories[storyNumber].choice1, for: .normal) //changes button title
+        choice2Button.setTitle(stories[storyNumber].choice2, for: .normal)
     }
+    
+//    func getStoryLabel() -> String {
+//        return storyLabel.text!
+//    }
 }
 
--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -9,17 +9,19 @@
 import Foundation
 
 struct Story {
-    let title: [String]
-    let choice1: [String]
-    let choice2: [String]
+    let title: String
+    let choice1: String
+    let choice2: String
     
-    init(title: [String], choice1: [String], choice2: [String]) {
+    init(title: String, choice1: String, choice2: String) {
         self.title = title
         self.choice1 = choice1
         self.choice2 = choice2
     }
 }
 
-var stories = Story(title: ["You see a fork in the road.", "You see a tiger", "You find a treasure chest."],
-                    choice1: ["Take a left.", "Shout for help.", "Open it."],
-                    choice2: ["Take a right.", "Play dead.", "Check for traps."])
+//let stories = [
+//    Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."),
+//    Story(title: "You see a tiger", choice1: "Shout for help.", choice2: "Play dead."),
+//    Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps.")
+//]
