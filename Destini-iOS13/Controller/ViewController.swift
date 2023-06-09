cca7e95bae7bdf7aa5ba40d2a0851cb7ca7cc42d
priscilla <priscillakung@gmail.com>
2023-06-09 12:05:03 -0700

    applied MVC

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -8,49 +8,31 @@
 
 import UIKit
 
-var storyNumber = 0 //keeps track of choices
-
-let stories = [
-    Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."),
-    Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
-    Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps.")
-]
-
 class ViewController: UIViewController {
 
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
+    var storyBrain = StoryBrain() //creates a new instance of StoryBrain to tap into StoryBrain
     
-    @IBAction func choiceMade(_ sender: UIButton) {
-       
+    @IBAction func choiceMade(_ sender: UIButton) { //when user presses a button
         let userChoice = sender.currentTitle!
-        
-        if userChoice == stories[storyNumber].choice1 {
-            storyNumber += 1
-        } else {
-            storyNumber += 2
-        }
+        storyBrain.nextStory(userChoice)
         updateUI()
     }
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        storyLabel.text = stories[storyNumber].title
+        storyLabel.text = "You see a fork in the road."
         choice1Button.setTitle("Take a left.", for: .normal)
         choice2Button.setTitle("Take a right.", for: .normal)
     }
     
     @objc func updateUI() {
-        storyLabel.text = stories[storyNumber].title //changes story label
-        choice1Button.setTitle(stories[storyNumber].choice1, for: .normal) //changes button title
-        choice2Button.setTitle(stories[storyNumber].choice2, for: .normal)
+        storyLabel.text = storyBrain.getStoryText() //changes story label
+        choice1Button.setTitle(storyBrain.getNextButton1(), for: .normal) //changes button title
+        choice2Button.setTitle(storyBrain.getNextButton2(), for: .normal)
     }
     
-//    func getStoryLabel() -> String {
-//        return storyLabel.text!
-//    }
 }
 
